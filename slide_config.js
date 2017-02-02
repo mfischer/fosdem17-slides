2bbd17c57f236c3085cde9f84dfaa61346e8a958
Moritz Fischer <moritz.fischer@ettus.com>
2017-02-02 11:27:42 +0100

    Initial version of slides
    This is still a work in progress an probably full
    of missing stuff.
    
    Signed-off-by: Moritz Fischer <moritz.fischer@ettus.com>

--- slide_config.js
+++ slide_config.js
@@ -1,18 +1,18 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    //},
+    title: '(Ab)using Google\'s Chromium EC<br>for your own projects',
+    subtitle: 'Building Franken Chrome Devices',
+    /*eventInfo: {
+      title: 'FOSDEM\'17',
+      date: '2/4/2017'
+    }, */
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/fosdem-gear.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -22,19 +22,13 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Moritz Fischer',
+    company: 'Senior Software Engineer<br>National Instruments',
+    gplus: 'http://plus.google.com/117055022771319709709',
+    twitter: '@fischmz',
+    www: 'http://www.ni.com',
+    github: 'http://github.com/mfischer'
   }/*, {
-    name: 'Second Name',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
   }*/]
 };
 
